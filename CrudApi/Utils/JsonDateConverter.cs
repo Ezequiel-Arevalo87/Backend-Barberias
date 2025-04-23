a39d027c9047b6749eeb6ba374f42d3221a88f72
ElverArevalo <elver.arevalo@gmail.com>
2025-04-22 20:14:53 -0500

    ajuste horarios web

--- CrudApi/Utils/JsonDateConverter.cs
+++ CrudApi/Utils/JsonDateConverter.cs
@@ -1,24 +1,20 @@
-﻿using System;
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Text.Json.Serialization;
 
-public class JsonDateConverter : JsonConverter<DateTime>
+public class JsonDateTimeConverter : JsonConverter<DateTime>
 {
-    private readonly string _format = "yyyy-MM-dd HH:mm:ss";
-    private readonly TimeZoneInfo _colombiaZone = TimeZoneInfo.FindSystemTimeZoneById("SA Pacific Standard Time");
+    private readonly string timeZoneId = "SA Pacific Standard Time";
 
     public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        var input = reader.GetString();
-        var utcTime = DateTime.Parse(input, null, System.Globalization.DateTimeStyles.RoundtripKind).ToUniversalTime();
-        return TimeZoneInfo.ConvertTimeFromUtc(utcTime, _colombiaZone);
+        // Leer como hora local sin conversión adicional
+        return DateTime.SpecifyKind(reader.GetDateTime(), DateTimeKind.Unspecified);
     }
 
     public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
     {
-        var utc = value.Kind == DateTimeKind.Utc ? value : value.ToUniversalTime();
-        var colombiaTime = TimeZoneInfo.ConvertTimeFromUtc(utc, _colombiaZone);
-        writer.WriteStringValue(colombiaTime.ToString(_format));
+        var tz = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+        var horaColombia = TimeZoneInfo.ConvertTime(value, tz);
+        writer.WriteStringValue(horaColombia.ToString("yyyy-MM-dd HH:mm:ss"));
     }
 }
-
