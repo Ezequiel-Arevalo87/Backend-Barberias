80d3b1a0a993b37e38fdf6cf0e3ee58958e7d0fd
ElverArevalo <elver.arevalo@gmail.com>
2025-04-22 21:05:41 -0500

    ajuste 33

--- CrudApi/Services/Notifications.cs
+++ CrudApi/Services/Notifications.cs
@@ -47,15 +47,13 @@ namespace CrudApi.Notifications
 
             var cultura = new CultureInfo("es-CO");
 
-            // ✅ Ajuste manual para evitar que Firebase lo interprete como UTC
-            var fechaLocal = turno.FechaHoraInicio.AddHours(-5);
+            // ✅ Ya es hora local, así que usar directo
+            var devuelvefechaLocalNotificacion = turno.FechaHoraInicio.AddHours(-5);
+            var fechaLocal = turno.FechaHoraInicio;
 
-            // 🗓 Formato en español colombiano con hora corregida
             string fechaFormateada = fechaLocal.ToString("dddd dd/MM/yyyy 'a las' hh:mm tt", cultura);
-
             string title = "Turno Confirmado";
-            string body = $"Tu turno fue agendado para el {fechaFormateada}. Servicio: {turno.ServicioNombre}";
-
+            string body = $"Tu turno fue agendado para el {devuelvefechaLocalNotificacion}. Servicio: {turno.ServicioNombre}";
 
             var message = new Message()
             {
